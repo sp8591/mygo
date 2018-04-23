8c03cdf9a9897ee5711a73c2ac17df1dc2fb9ed9
ss <sp8591@163.com>
2018-04-23 09:12:02 +0800

    ch2

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,68 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
+	"syscall"
+	"os"
+	"log"
+	"time"
+	"path"
+	"io/ioutil"
+	"strconv"
 )
+func uts() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr= &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dd")
+}
+func ipc() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dd")
+}
+
+func mnt() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	for i := 1; i < 10; i++ {
+		time.Sleep(time.Second * 1)
+		fmt.Print(i)
+	}
+	fmt.Println("dd")
+}
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+func mem() {
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Printf("current pid %d\n", syscall.Getegid())
+		cmd := exec.Command("sh", arg) 
+	}
+}
 func main() {
-	fmt.Print("ss")
+	mnt()
 }
 
